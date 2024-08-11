e6554e47b07b50b468bcb08a323100295d56b35f
mhebbalkar <138763358+mhebbalkar@users.noreply.github.com>
2024-08-11 11:31:32 +0530

    Add files via upload

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Transactions;
+
+namespace Demo.Models
+{
+	public class TransactionDbContext:DbContext
+	{
+		public TransactionDbContext(DbContextOptions<TransactionDbContext> options):base(options)
+		{
+
+
+
+
+		}
+		public DbSet<Transaction> Transactions { get; set; }
+	}
+}
+ 
